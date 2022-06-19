224d7e289e5326218e42ec8a068a8f6067586e54
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2022-06-19 14:48:25 +0500

    Add files via upload

---
+++ ProductOfTwoNumbersWithoutOperator.java
@@ -0,0 +1,17 @@
+public class  ProductOfTwoNumbersWithoutOperator{
+
+      static int  product(int a, int b){
+	
+		int sum = 0;
+		for(int i =0; i<b; i++){
+		
+			sum +=a;
+		
+		}
+		return sum;
+	}
+	public static void main(String[] s){
+
+
+	}
+}
