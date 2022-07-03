80f6c35d670f6d1a5ff2d7b88cd0f0c24bf90bc9
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2022-07-04 00:00:24 +0500

    Update ZeroMatrix.java

--- ZeroMatrix.java
+++ ZeroMatrix.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class ZeroMatrix{
-
+	 
 	public static void makeZeros(int row, int col, int[][] matrix){
 	
 		//if rows and column length same then this should work otherwise
@@ -8,7 +8,7 @@ public class ZeroMatrix{
 		//run another loop for columns
 		for(int j=0;j<matrix[0].length;j++){
 		          matrix[row][j]=0;
-		         matrix[j][col]=0;	 
+		          matrix[j][col]=0;	 
 			
 		}
 
@@ -23,29 +23,39 @@ public class ZeroMatrix{
 	public static void main(String... args){
 	
 		int[][] matrix=new int[][]{
-			{1,3,4,0},
+			{0,3,4,0},
 			{2,3,0,3},
 			{1,3,4,5},
 			{10,30,4,1}
 
 		};
 
-		Map<Integer,Boolean> memo = new HashMap<>();
+		boolean[][] matrixMarker=new boolean[matrix.length][matrix[0].length];
+
 		for(int i=0;i<matrix.length;i++){
 		
 			for(int j=0; j<matrix[0].length;j++){
 			
-				if(matrix[i][j]==0 && (memo.get(i)==null && memo.get(j)==null) ){
-				 memo.put(i,true);
-				 memo.put(j,true);
-			         makeZeros(i,j,matrix);
-				 break;
+				if(matrix[i][j]==0 ){
+				 matrixMarker[i][j]=true;
 				}
 				
 			}
 			
 		}
 	
+	     for(int i=0;i<matrix.length;i++){
+
+                        for(int j=0; j<matrix[0].length;j++){
+
+                               if(matrixMarker[i][j]==true){
+			       makeZeros(i,  j,  matrix);
+			       }
+
+                        }
+
+                }
+
 
 		  for(int i=0;i<matrix.length;i++){
 
